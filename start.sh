5dc2805c772fadda231ae691e9db81415fe24beb
actionchain <actionchain@chainguard.dev>
2026-06-05 12:32:37 +0000

    Update actions-js/push@v1.1

---
+++ start.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e
+
+INPUT_AUTHOR_EMAIL=${INPUT_AUTHOR_EMAIL:-'github-actions[bot]@users.noreply.github.com'}
+INPUT_AUTHOR_NAME=${INPUT_AUTHOR_NAME:-'github-actions[bot]'}
+INPUT_COAUTHOR_EMAIL=${INPUT_COAUTHOR_EMAIL:-''}
+INPUT_COAUTHOR_NAME=${INPUT_COAUTHOR_NAME:-''}
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_TAGS=${INPUT_TAGS:-false}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+_FORCE_OPTION=''
+REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+
+echo "Push to branch $INPUT_BRANCH";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+if ${INPUT_FORCE}; then
+    _FORCE_OPTION='--force'
+fi
+
+if ${INPUT_TAGS}; then
+    _TAGS='--tags'
+fi
+
+cd "${INPUT_DIRECTORY}"
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+git config http.sslVerify false
+git config --local user.email "${INPUT_AUTHOR_EMAIL}"
+git config --local user.name "${INPUT_AUTHOR_NAME}"
+
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+git add -A
+
+if [ -n "${INPUT_COAUTHOR_EMAIL}" ] && [ -n "${INPUT_COAUTHOR_NAME}" ]; then
+    git commit -m "chore: autopublish ${timestamp}
+    
+
+Co-authored-by: ${INPUT_COAUTHOR_NAME} <${INPUT_COAUTHOR_EMAIL}>" || exit 0
+else
+    git commit -m "chore: autopublish ${timestamp}" || exit 0
+fi
+
+git push "${remote_repo}" HEAD:"${INPUT_BRANCH}" --follow-tags $_FORCE_OPTION $_TAGS;
